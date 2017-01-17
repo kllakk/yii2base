e511454ae1782f6c80f241c0a4b7e6b52e13ce3d
BPROG2\Сергей <petrovsa83@mail.ru>
2017-01-17 12:37:14 +0300

    установка препроцессора npm init npm install gulp --save-dev npm install gulp-sass --save-dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
     console.log('hello gulp');
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./web/css/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./web/css/'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./web/css/scss/**/*.scss', ['sass']);
 });
