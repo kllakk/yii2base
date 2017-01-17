96a369b90ab8ff45d1c995e5d389894b7d629733
BPROG2\Сергей <petrovsa83@mail.ru>
2017-01-18 01:38:49 +0300

    файл конфига webpack и файлы для формирования  web/assets npm run build

---
+++ web/js/vendor.js
@@ -0,0 +1,6 @@
+window.jQuery = window.$ = require('jquery');
+require('bootstrap');
+require('yii2-pjax');
+require('yii');
+require('yii.validation');
+require('yii.activeForm');
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BowerWebpackPlugin = require('bower-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const config =  {
+
+    entry: {
+        site: './web/js/',
+    },
+
+    output: {
+        path: path.join(__dirname, './web/assets'),
+        filename: '[name].[chunkhash].js',
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+            },
+            {
+                test: /\.(woff2|woff|svg|ttf|eot)([\?]?.*)$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+        ],
+
+        noParse: [
+            /\.min\.js/,
+        ],
+    },
+
+    resolve: {
+        alias: {
+            css: __dirname + '/web/css',
+        },
+        modulesDirectories: [
+            'node_modules',
+            __dirname + '/vendor/yiisoft/yii2/assets',
+        ],
+    },
+
+    plugins: [
+        new BowerWebpackPlugin({
+            modulesDirectories: ['./vendor/bower'],
+            manifestFiles: ['bower.json', '.bower.json'],
+            includes: /.*/,
+            excludes: /.*\.less$/,
+        }),
+        new ExtractTextPlugin('[name].[contenthash].css'),
+        new ManifestPlugin(),
+    ],
+};
+
+
+config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+            warnings: false,
+        },
+    })
+);
+
+module.exports = config;
