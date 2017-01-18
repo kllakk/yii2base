f4e7e57c33405e7e606df0553ce10f02396c0bc8
BPROG2\Сергей <petrovsa83@mail.ru>
2017-01-18 16:56:21 +0300

    всякие пробы и нюансы

--- modules/api/modules/v1/models/Settings.php
+++ modules/api/modules/v1/models/Settings.php
@@ -2,6 +2,17 @@
 
 namespace app\modules\api\modules\v1\models;
 
+use yii\helpers\Url;
+
 class Settings extends \app\models\Settings
 {
+    public function fields()
+    {
+        $fields = parent::fields();
+
+        // remove fields that contain sensitive information
+        unset($fields['id']);
+
+        return $fields;
+    }
 }
